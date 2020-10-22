886935ff79fa96f622941a52ff192bc3b44ba73a
Alexander van den Herik <alexander.van.den.herik2@hva.nl>
2020-10-22 23:51:59 +0200

    Added FEB back and made it able to hide

--- app/src/main/java/nl/herika/app/MainActivity.kt
+++ app/src/main/java/nl/herika/app/MainActivity.kt
@@ -5,18 +5,40 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        navController = findNavController(R.id.nav_host_fragment)
 
+        fab.setOnClickListener {
+            navController.navigate(
+                R.id.action_FirstFragment_to_SecondFragment
+            )
+        }
+
+        fabToggler()
     }
 
+    private fun fabToggler() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in arrayOf(R.id.FirstFragment)) {
+                fab.show()
+            } else {
+                fab.hide()
+            }
+        }
+    }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
