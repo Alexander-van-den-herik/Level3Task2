84483e8b90bf2d756b0330038eb0c4fde3a332b7
Alexander van den Herik <alexander.van.den.herik2@hva.nl>
2020-10-22 23:47:05 +0200

    portal add fragment done??

--- app/src/main/java/nl/herika/app/PortalAddFragment.kt
+++ app/src/main/java/nl/herika/app/PortalAddFragment.kt
@@ -7,25 +7,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_portal_add.*
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class PortalAddFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_portal_add, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        btn_add_portal.setOnClickListener {
+            onAddPortal()
         }
+    }
+
+    private fun onAddPortal() {
+        //TODO(Add logic about adding a portal)
     }
 }
--- app/src/main/java/nl/herika/app/PortalOverviewFragment.kt
+++ app/src/main/java/nl/herika/app/PortalOverviewFragment.kt
@@ -1,17 +1,32 @@
 package nl.herika.app
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_portal_overview.*
+import nl.herika.app.data.Portal
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class PortalOverviewFragment : Fragment() {
+
+    private val portals = arrayListOf<Portal>()
+    private val portalAdapter = PortalAdapter(portals) { portal: Portal ->
+        portalClicked(
+            portal
+        )
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +39,45 @@ class PortalOverviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        rv_portal_cards.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+        rv_portal_cards.adapter = portalAdapter
+
+        createItemTouchHelper().attachToRecyclerView(rv_portal_cards)
     }
+
+    private fun portalClicked(portal: Portal) {
+        val url = portal.url
+
+        val browserIntent =
+            Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                portals.removeAt(position)
+                portalAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+//    private fun observeAddReminderResult() {
+//        setFragmentResultListener(PORTAL) { _, bundle ->
+//            bundle.getParcelable<Portal>(PORTAL)?.let {
+//                portals.add(it)
+//            } ?: Log.e("PortalCard", "No PortalCard received!")
+//        }
+//    }
 }
